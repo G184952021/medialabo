cb56573f7a35fa85da1e15d3840b724472504449
G184952021 <218495@st.takushoku-u.ac.jp>
2022-05-19 15:11:48 +0900

    a

--- kazuate/kazuate.js
+++ kazuate/kazuate.js
@@ -3,30 +3,39 @@ console.log('答え:' +kotae);
 let kaisu = 0;
 let a=0;
 hantei();
-hantei();
-hantei();
-hantei();
 function hantei(){
     let yoso =4;
     let b=document.querySelector('span#yoso');
     b.textContent=yoso;
     kaisu=kaisu+1;
     let c=document.querySelector('span#kaisu');
+    c.textContent=kaisu;
+    
     if(yoso===kotae&&a<1){
-        let kotae="('正解です。おめでとう！')";
+        let kotae="正解です。おめでとう！";
+        let d=document.querySelector('p#result');
+        d.textContent=kotae;
         a=a+1;
         kaisu=kaisu+2;
     }
     if(yoso<kotae&&kaisu<3){
-        console.log('まちがい、答えはもっと大きいですよ。');
+        let kotae="まちがい、答えはもっと大きいですよ。";
+        let d=document.querySelector('p#result');
+        d.textContent=kotae;
     }
     if(yoso>kotae&&kaisu<3){
-        console.log('まちがい、答えはもっと小さいですよ。');
+        let kotae="まちがい、答えはもっと小さいですよ。";
+        let d=document.querySelector('p#result');
+        d.textContent=kotae;
     }
     if(kaisu===3&&yoso!==kotae){
-        console.log('まちがい、残念でした答えは'+'kotae'+'です。');
+        let kotae="まちがい、残念でした答えは'+'kotae'+'です。";
+        let d=document.querySelector('p#result');
+        d.textContent=kotae;
     }
     if(kaisu>=4){
-        console.log('答えは'+kotae+'でした。　すでにゲームは終わっています');
+        let kotae="答えは'+kotae+'でした。　すでにゲームは終わっています";
+        let d=document.querySelector('p#result');
+        d.textContent=kotae;
     }
   }
