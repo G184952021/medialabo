1e2833083cd5916ec6b8c7e36a1eb034b7234155
G184952021 <218495@st.takushoku-u.ac.jp>
2022-06-02 23:13:16 +0900

    a

--- project/weather.js
+++ project/weather.js
@@ -1,53 +1,6 @@
-let data = {
-  "coord": {
-    "lon": 116.3972,
-    "lat": 39.9075
-  },
-  "weather": [
-    {
-      "id": 803,
-      "main": "Clouds",
-      "description": "曇りがち",
-      "icon": "04d"
-    }
-  ],
-  "base": "stations",
-  "main": {
-    "temp": 9.94,
-    "feels_like": 8.65,
-    "temp_min": 9.94,
-    "temp_max": 9.94,
-    "pressure": 1022,
-    "humidity": 14,
-    "sea_level": 1022,
-    "grnd_level": 1016
-  },
-  "visibility": 10000,
-  "wind": {
-    "speed": 2.65,
-    "deg": 197,
-    "gust": 4.84
-  },
-  "clouds": {
-    "all": 53
-  },
-  "dt": 1646542386,
-  "sys": {
-    "type": 1,
-    "id": 9609,
-    "country": "CN",
-    "sunrise": 1646520066,
-    "sunset": 1646561447
-  },
-  "timezone": 28800,
-  "id": 1816670,
-  "name": "北京市",
-  "cod": 200
-};
-
-////////// 課題3-2 ここからプログラムを書こう
 let s = document.querySelector('button#search');
 s.addEventListener('click', kensaku);
+
 function kensaku(){
   var element = document.getElementById( "target" ).value ;
   let url='https://www.nishita-lab.org/web-contents/jsons/openweather/'+element+'.json';
@@ -56,6 +9,7 @@ function kensaku(){
         .catch(showError)
         .then(finish);
 }
+let kumori='Clouds';
 
 function showResult(resp) {
   let data = resp.data;
@@ -88,8 +42,29 @@ function showResult(resp) {
     
     let e=document.querySelector('h1#tenki');
     e.textContent=data.weather[0].main;
-    if(data.weather[0].main==='Clouds'){
-     
+
+    if(data.weather[0].main=='Clouds'){
+      var sampleElement = document.getElementById('random');
+      sampleElement.classList.remove('addColor2','addColor3','addColor4');
+      document.getElementById("random").classList.add("addColor");
+    }
+    
+    else if(data.weather[0].main=='Clear'){
+      var sampleElement = document.getElementById('random');
+      sampleElement.classList.remove('addColor','addColor3','addColor4');
+      document.getElementById("random").classList.add("addColor2");
+    }
+    
+    else if(data.weather[0].main=='Rain'){
+      var sampleElement = document.getElementById('random');
+      sampleElement.classList.remove('addColor','addColor3','addColor2');
+      document.getElementById("random").classList.add("addColor4");
+    }
+    
+    else if(data.weather[0].main=='Mist'){
+      var sampleElement = document.getElementById('random');
+      sampleElement.classList.remove('addColor','addColor2','addColor4');
+      document.getElementById("random").classList.add("addColor3");
     }
 }
 function showError(err) {
