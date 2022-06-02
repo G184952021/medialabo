c3cea6ec759f12c473546d822b26c8947a9f4672
G184952021 <218495@st.takushoku-u.ac.jp>
2022-06-02 15:28:53 +0900

    a

--- project/weather.js
+++ project/weather.js
@@ -48,22 +48,53 @@ let data = {
 ////////// 課題3-2 ここからプログラムを書こう
 let s = document.querySelector('button#search');
 s.addEventListener('click', kensaku);
-
 function kensaku(){
-var element = document.getElementById( "target" ) ;
-// 選択状態の項目の値を取得,ここから下のdataはfor文で数字を変えたい。
-var d = element.value ;
-if(d===data.name){
-let a=document.querySelector('h1#country');
-a.textContent=d;
-
-let b=document.querySelector('span#highkion');
-b.textContent=data.main.temp_max;
-
-let c=document.querySelector('span#lowkion');
-c.textContent=data.main.temp_min;
-
-let e=document.querySelector('h1#tenki');
-e.textContent=data.weather[main];
+  var element = document.getElementById( "target" ).value ;
+  let url='https://www.nishita-lab.org/web-contents/jsons/openweather/'+element+'.json';
+  axios.get(url)
+        .then(showResult)
+        .catch(showError)
+        .then(finish);
 }
+
+function showResult(resp) {
+  let data = resp.data;
+  if (typeof data === 'string') {
+      data = JSON.parse(data);
+  }
+    let obj = document.getElementById('target');
+    let idx = obj.selectedIndex;
+    let txt  = obj.options[idx].text;
+    let a=document.querySelector('h1#country');
+    a.textContent=txt;
+
+    let ko='最高気温:';
+    let f=document.querySelector('span#ooo');
+    f.textContent=ko;
+    let ki='℃';
+    let g=document.querySelector('span#do');
+    g.textContent=ki;
+    let b=document.querySelector('span#highkion');
+    b.textContent=data.main.temp_max;
+
+    let mu='最低気温:';
+    let h=document.querySelector('span#oooo');
+    h.textContent=mu;
+    let ke='℃';
+    let i=document.querySelector('span#doo');
+    i.textContent=ke;
+    let c=document.querySelector('span#lowkion');
+    c.textContent=data.main.temp_min;
+    
+    let e=document.querySelector('h1#tenki');
+    e.textContent=data.weather[0].main;
+    if(data.weather[0].main==='Clouds'){
+     
+    }
+}
+function showError(err) {
+  console.log(err);
+}
+function finish() {
+  console.log('Ajax 通信が終わりました');
 }
